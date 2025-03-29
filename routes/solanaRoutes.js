b6b7d3e75ba8f343cb15aaaa689d3c6a199330f1
Pranav Dhadwal <65543718+Storryy@users.noreply.github.com>
2025-03-30 00:21:03 +0530

    plsyar

--- routes/solanaRoutes.js
+++ routes/solanaRoutes.js
@@ -512,7 +512,18 @@ router.get('/generate/:address', async (req, res) => {
       quality: 100,
       type: 'png',
       puppeteerArgs: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-dev-shm-usage',
+          '--disable-accelerated-2d-canvas',
+          '--no-first-run',
+          '--no-zygote',
+          '--single-process',
+          '--disable-gpu'
+        ],
+        headless: true,
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || null
       }
     });
     
